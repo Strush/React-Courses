2fc8cfc0d5a98d691a6eedd1d1847e78e1fffef1
Petru <vinagapetru11@gmail.com>
2020-07-05 16:50:45 +0300

    Primul commit

---
+++ js/index.js
@@ -0,0 +1,35 @@
+
+const obj = {
+    name: 'Petru',
+    id: 1,
+    age: 21,
+}
+
+console.log(obj.age);
+
+const arr = ['Petru','Front',21];
+console.log(arr[2]);
+
+
+let name = 'Petrru';
+console.log(name);
+
+
+console.log(names);
+var names = 'Piter';
+
+
+
+// const person = confirm('AI FOST ?');
+// console.log(person);
+
+// const per = +prompt('Ai 18 ani', '');
+// console.log(per + 5);
+
+
+const user = []
+ user[0] = prompt('Name:','');
+ user[1] = prompt('Ani','');
+ user[2] = prompt('Prenume','')
+
+ document.write(user);
